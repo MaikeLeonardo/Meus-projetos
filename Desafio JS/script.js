ba91a1598052843480f8fdad58eb5056b089c1dc
Maike Leonardo <118013894+MaikeLeonardo@users.noreply.github.com>
2023-06-02 13:28:56 -0300

    programa de Operações matemáticas

---
+++ Desafio JS/script.js
@@ -0,0 +1,17 @@
+
+let numberOne = prompt("Digite o 1° numero")
+let numberTwo = prompt("Digite o 2° numero")
+let sun = Number(numberOne) + Number(numberTwo)
+let sub = Number(numberOne) - Number(numberTwo)
+let mult = Number(numberOne) * Number(numberTwo)
+let divi = Number(numberOne) / Number(numberTwo)
+let res = Number(numberOne) % Number(numberTwo)
+
+
+alert ("A soma dos dois é igual á : " + sun)
+alert ("A subtração dos dois é igual á : " + sub)
+alert ("A multiplicação dos dois é igual á : " + mult)
+alert ("A divisão dos dois é igual á : " + divi)
+alert ("O resto dos dois é igual á : " + res)
+
+
